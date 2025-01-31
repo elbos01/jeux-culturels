9244940d446b73c9ca1fb3c71d70646b3f8ba220
elbos01 <zhrmagik@gmail.com>
2025-01-31 02:46:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemple de fonction JavaScript pour l'interactivité
+document.addEventListener('DOMContentLoaded', function () {
+    // Ajoutez des interactions ici
+    console.log('Site chargé !');
+});
